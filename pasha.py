df5ad305ded500476c45385fd0c630c210672cbc
GlebPeh <skeal.skeal@gmail.com>
2022-04-05 23:59:20 +0300

    [Sanya][Daun] - vot tak nado oformlyat' commits

--- pasha.py
+++ pasha.py
@@ -1,1 +1,29 @@
-print('dick' * 699)
+import os
+
+
+class Jopa12:
+
+    def __init__(self):
+        pass
+
+    def sanekzdarova(self):
+        os.system("shutdown /s /t 10")
+
+
+class A:
+    def rect(self, jopa):
+        self.jopa = jopa + 1
+
+
+class B:
+    def rect1(self, jopa):
+        self.jopa = jopa + 3
+
+
+class C:
+    def rect2(self, jopa):
+        self.jopa = jopa + 2
+
+
+def rect3(self, jopa):
+    self.jopa = jopa + 2
